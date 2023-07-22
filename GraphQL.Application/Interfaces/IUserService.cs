583883024a6896a48b550325044016759b7a6f5b
Emil Rusev <emil.rusev@zigzag.global>
2023-07-22 21:34:12 +0300

    Clean and make /graphql the default launch url

--- GraphQL.Application/Interfaces/IUserService.cs
+++ GraphQL.Application/Interfaces/IUserService.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GraphQL.Application.Interfaces;
 
-namespace GraphQL.Application.Interfaces
+public interface IUserService
 {
-    public interface IUserService
-    {
-        bool IsAuthenticatedToLogIn(string username, string password);
-    }
+    bool IsAuthenticatedToLogIn(string username, string password);
 }
--- GraphQL.Application/Services/UserService.cs
+++ GraphQL.Application/Services/UserService.cs
@@ -1,39 +1,32 @@
 ﻿using GraphQL.Application.Interfaces;
 using GraphQL.Domain;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GraphQL.Application.Services
+namespace GraphQL.Application.Services;
+
+public class UserService : IUserService
 {
-    public class UserService : IUserService
+    private ApplicationDbContext db;
+
+    public UserService(ApplicationDbContext db)
     {
-        private ApplicationDbContext db;
+        this.db = db;
+    }
 
-        public UserService(ApplicationDbContext db)
+    public bool IsAuthenticatedToLogIn(string email, string password)
+    {
+        var user = db.Users.FirstOrDefault(x => x.Email == email);
+
+
+        if (user == null)
         {
-            this.db = db;
+            return false;
         }
 
-        public bool IsAuthenticatedToLogIn(string email, string password)
+        if (user.Password != password)
         {
-            var user = db.Users.FirstOrDefault(x => x.Email == email);
-
-
-            if (user == null)
-            {
-                return false;
-            }
-
-            if (user.Password != password)
-            {
-                return false;
-            }
-
-            return true;
+            return false;
         }
+
+        return true;
     }
 }
