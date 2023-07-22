4f83103f36edf7032fa928d35fab5694004f5272
Emil Rusev <azsumemi@gmail.com>
2023-07-22 21:44:01 +0300

    Remove usings

--- GraphQL.Domain/ApplicationDbContext.cs
+++ GraphQL.Domain/ApplicationDbContext.cs
@@ -1,28 +1,22 @@
 ﻿using GraphQL.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GraphQL.Domain
+namespace GraphQL.Domain;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext()
     {
-        public ApplicationDbContext()
-        {
-        }
+    }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
 
-        public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
     }
 }
